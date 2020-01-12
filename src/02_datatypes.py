ab697118a8b85278a043576c944a7c471971c7b4
Lucas Bazemore <lucasabazemore@gmail.com>
2020-01-12 14:41:10 -0600

    first set of python steps

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -1,12 +1,13 @@
 """
-Python is a strongly-typed language under the hood, which means 
+Python is a strongly-typed language under the hood, which means
 that the types of values matter, especially when we're trying
-to perform operations on them. 
+to perform operations on them.
 
 Note that if you try running the following code without making any
 changes, you'll get a TypeError saying you can't perform an operation
 on a string and an integer.
 """
+import math
 
 x = 5
 y = "7"
@@ -14,8 +15,9 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(x) + y)
